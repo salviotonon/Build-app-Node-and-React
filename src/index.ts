57d3a7b4a12e54cac46a8628a408ba25f36052af
salviotonon <salvio.tonon@gmail.com>
2022-11-15 22:28:11 -0300

    build backend aplication

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import path from 'node:path';
+import express from 'express';
+import mongoose from 'mongoose';
+
+import { router } from './router';
+
+
+mongoose.connect('mongodb://127.0.0.1:27017')
+  .then(() => {
+    const app = express();
+    const port = 3002;
+
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+    app.use(express.json());
+    app.use(router);
+
+    app.listen(port, () => {
+      console.log(`🚀 server is running on http://127.0.0.1:${port}`);
+    });
+  })
+  .catch(() => console.log('Erro ao conectar no mongodb'));
+
+
+
